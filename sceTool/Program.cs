44e2c22fab7b1dad7b81aeb48cc37e2f0d517c1f
shikwi <crpandas@163.com>
2026-01-02 01:38:20 +0800

    scb2txt

---
+++ sceTool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using sceTool;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        foreach (var arg in args)
+        {
+            var scb = new scbFile();
+            if (Path.GetExtension(arg).ToLower() == ".scb")
+            {
+                var filename = Path.GetFileNameWithoutExtension(arg) + ".txt";
+                var outpath = Path.Combine(Path.GetDirectoryName(arg)!, filename);
+                scb.ExportScb(arg, outpath);
+                Console.WriteLine($"Exported {filename}");
+            }
+        }
+
+        Console.WriteLine($"Convert Finish......");
+        Console.ReadKey();
+    }
+}
---
+++ sceTool/scbFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sceTool
+{
+    public class scbFile
+    {
+        private const uint SCB_MAGIC = 0X4B504C47;  //GLPK
+
+        private struct scbHeader
+        {
+            public uint magic;
+            public uint UnpackSize;
+            public uint EncryptFlag;
+        }
+
+        public void ExportScb(string filepath, string outpath)
+        {
+            using (BinaryReader br = new BinaryReader(File.OpenRead(filepath)))
+            {
+                var header = new scbHeader
+                {
+                    magic = br.ReadUInt32(),
+                    UnpackSize = br.ReadUInt32(),
+                    EncryptFlag = br.ReadUInt32(),
+                };
+
+                if (header.magic != SCB_MAGIC)
+                    throw new Exception("Not valid scb file");
+
+                var sceData = br.ReadBytes((int)br.BaseStream.Length - 0xC);
+                if (header.EncryptFlag != 0) NOTDecrypt(sceData);
+
+                sceData = gfbTool.Util.LzssDecompress(sceData, header.UnpackSize);
+                var outStrings = Encoding.GetEncoding("shift_jis").GetString(sceData);
+                File.WriteAllText(outpath, outStrings, new UTF8Encoding(false));
+            }
+        }
+
+        private void NOTDecrypt(byte[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)(~data[i]);
+            }
+        }
+
+    }
+}
